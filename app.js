2f4459a4fc3a9e1648a771299922f6395956ea9e
Shreyank Puthran <shreyankputhran@gmail.com>
2026-03-10 15:39:25 +0530

    removed unecessary code

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 import express from "express";
-import cors from "cors";
 
 import walletRoutes from "./routes/walletRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use(walletRoutes);
